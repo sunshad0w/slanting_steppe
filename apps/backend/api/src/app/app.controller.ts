ff9b024f52b849238d7d9dd1c4fb1957f0a13e47
sunshad0w <sunshad0w.0leg@gmail.com>
2022-12-27 18:54:01 +0800

    add typeorm

--- apps/backend/api/src/app/app.controller.ts
+++ apps/backend/api/src/app/app.controller.ts
@@ -1,13 +1,77 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller } from '@nestjs/common';
 
-import { AppService } from './app.service';
+// import { User as UserModel, Post as PostModel, Prisma } from '@prisma/client';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor() {}
 
-  @Get()
-  getData() {
-    return this.appService.getData();
+  /* @Get('post/:id')
+  async getPostById(@Param('id') id: string): Promise<PostModel> {
+    return this.prismaService.post.findUnique({ where: { id: Number(id) } });
   }
+
+  @Get('users')
+  async getAllUsers(): Promise<UserModel[]> {
+    return this.prismaService.user.findMany();
+  }
+
+  @Post('post')
+  async createDraft(
+    @Body() postData: { title: string; content?: string; authorEmail: string }
+  ): Promise<PostModel> {
+    const { title, content, authorEmail } = postData;
+    return this.prismaService.post.create({
+      data: {
+        title,
+        content,
+        author: {
+          connect: { email: authorEmail },
+        },
+      },
+    });
+  }
+
+  @Post('signup')
+  async signupUser(
+    @Body()
+    userData: {
+      name?: string;
+      email: string;
+      posts?: Prisma.PostCreateInput[];
+    }
+  ): Promise<UserModel> {
+    const postData = userData.posts?.map((post) => {
+      return { title: post?.title, content: post?.content };
+    });
+    return this.prismaService.user.create({
+      data: {
+        name: userData?.name,
+        email: userData.email,
+        posts: {
+          create: postData,
+        },
+      },
+    });
+  }
+
+  @Put('publish/:id')
+  async togglePublishPost(@Param('id') id: string): Promise<PostModel> {
+    const postData = await this.prismaService.post.findUnique({
+      where: { id: Number(id) },
+      select: {
+        published: true,
+      },
+    });
+
+    return this.prismaService.post.update({
+      where: { id: Number(id) || undefined },
+      data: { published: !postData?.published },
+    });
+  }
+
+  @Delete('post/:id')
+  async deletePost(@Param('id') id: string): Promise<PostModel> {
+    return this.prismaService.post.delete({ where: { id: Number(id) } });
+  }*/
 }
